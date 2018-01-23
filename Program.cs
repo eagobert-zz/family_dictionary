865a3f4f1ab821b5b85ba8c53c8b202124851ba2
eagobert <eagobert@hotmail.com>
2018-01-22 18:17:04 -0600

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace family_dictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("");
+
+            //Define a Dictionary that contains information about several members of your family.
+
+            Dictionary<string, Dictionary <string, string>> myFamily = new Dictionary<string, Dictionary <string, string>>();
+        }
+    }
+}
